cdc5bd3c79430a8e81b6cb3beb253af927b4363d
Bruno Rodrigues <brunorodrigues@gmail.com>
2023-05-22 19:09:38 -0300

    teclado-virtual

---
+++ teclado.js
@@ -0,0 +1,9 @@
+//function aperta(lt){
+//    document.getElementById("aperta").innerHTML = "Você apertou a letra "+lt;
+//}
+function solta(lt){
+    document.getElementById("solta").innerHTML = `Você soltou a letra ${lt}`;
+}
+function solta(lt){
+    document.getElementById("solta").innerHTML = "Você soltou a letra "+lt;
+}
